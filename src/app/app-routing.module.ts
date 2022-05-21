c291eafeb97cdb3719f4eb846f3c641d070b1fee
AMurillo-UTM170072 <aureliomurillo12@gmail.com>
2022-05-21 13:40:35 -0500

    refcator(app):advanced

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BongsGlassComponent } from './Articulos/bongs-glass/bongs-glass.component';
+import { WaxComponent } from './Articulos/wax/wax.component';
 import { HomeComponent } from './landPage/home/home.component';
 import { PageNotFoundComponent } from './landPage/page-not-found/page-not-found.component';
-
 
 //modulos de articulos 
 const routes: Routes = [
   {path: 'Home', component: HomeComponent },
   {path: 'bongGlass', component: BongsGlassComponent },
-  {path: 'wax', component: BongsGlassComponent },
+  {path: 'wax', component: WaxComponent },
   {path: '**', redirectTo:'Home'},
   { path: '**', component: PageNotFoundComponent },
 ];
